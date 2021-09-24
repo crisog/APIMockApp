89b7f1320d88c3b1f27a76a75970fc74fd2b00a0
crisog <ortega.cpp@gmail.com>
2021-09-24 15:56:28 -0400

    Init MVVM

--- APIMockApp/App.xaml.cs
+++ APIMockApp/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using APIMockApp.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace APIMockApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new HomePage();
         }
 
         protected override void OnStart()
